75bd799b1de70df93101bdec86075071daa2d4f4
Jose <jose@acer-barebone>
2025-09-17 00:11:02 +0200

    iniciando lección 2

---
+++ Leccion_2/1_listas.py
@@ -0,0 +1,6 @@
+frutas = ["manzana", "banana", "cereza"]
+print(frutas[0])         # acceso: "manzana"
+frutas.append("pera")    # añadir
+frutas.remove("banana")  # eliminar
+frutas[1] = "kiwi"       # modificar
+print(frutas)
